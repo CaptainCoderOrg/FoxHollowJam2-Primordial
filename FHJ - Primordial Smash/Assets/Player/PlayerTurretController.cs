1cafd8b3d81dc925e17bde9fd2183131ccc50218
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-07-06 17:14:20 -0700

    feat: Add mouse turret controls.

--- FHJ - Primordial Smash/Assets/Player/PlayerTurretController.cs
+++ FHJ - Primordial Smash/Assets/Player/PlayerTurretController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.MPE;
 using UnityEngine;
 
@@ -12,6 +13,8 @@ public class PlayerTurretController : MonoBehaviour
     public float MagnitudeDeadZone = 0.1f;
     [field: SerializeField]
     public bool CanFire { get; private set; } = true;
+    [field: SerializeField]
+    public Vector2 MousePosition { get; private set; }
 
     void Awake()
     {
@@ -21,15 +24,41 @@ public class PlayerTurretController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HandleAnalog();
+        HandleMouse();
+    }
+
+    private void HandleMouse()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        if (mousePosition != MousePosition)
+        {
+            MousePosition = mousePosition; 
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(MousePosition);
+            DirectionInput = (Vector2.MoveTowards(transform.position, worldPosition, 1) - (Vector2)transform.position).normalized;
+            Debug.DrawLine(transform.position, DirectionInput + (Vector2)transform.position);
+            Player.Turret.transform.rotation = Quaternion.FromToRotation(Vector2.up, DirectionInput);
+        
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Fire();
+        }
+    }
+
+    private void HandleAnalog()
+    {
+        
         Vector2 input = default;
         input.x = Input.GetAxis("TurretHorizontal");
         input.y = Input.GetAxis("TurretVertical");
+        if (input.x == 0 && input.y == 0) { return; }
+        
         DirectionInput = input;
         if (DirectionInput.magnitude > MagnitudeDeadZone)
         {
             Player.Turret.transform.rotation = Quaternion.FromToRotation(Vector2.up, DirectionInput.normalized);
             Fire();
-            
         }
     }
 
